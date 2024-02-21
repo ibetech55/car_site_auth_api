14d3279f50dee977b635f588ca7d7c00c638854a
pibewiro <pibewiro@yahoo.com>
2024-02-20 22:08:46 -0300

    chore: add domain to cookie config

--- src/Controllers/Auth/LoginController/index.ts
+++ src/Controllers/Auth/LoginController/index.ts
@@ -7,30 +7,33 @@ class LoginController {
 
   constructor(loginUseCase: LoginUseCase) {
     this._loginUseCase = loginUseCase;
-    console.log(COOKIE_DOMAIN, 999)
-
   }
 
   async handle(request: Request, response: Response) {
     const data = await this._loginUseCase.execute(request.body);
-    response.cookie("auth_token", data.auth_token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "none",
-      maxAge: - 1,
-      domain: COOKIE_DOMAIN,
-      path: '/',
-      expires: new Date(Date.now() + 900000)
-    });
-    response.cookie("login_token", data.login_token, {
-      httpOnly: false,
-      secure: false,
-      sameSite: "none",
-      maxAge: - 1,
-      domain: COOKIE_DOMAIN,
-      path: '/',
-      expires: new Date(Date.now() + 900000)
-    });
+    // response.cookie("auth_token", data.auth_token, {
+    //   httpOnly: true,
+    //   secure: false,
+    //   sameSite: "none",
+    //   maxAge: - 1,
+    //   domain: COOKIE_DOMAIN,
+    //   path: '/',
+    //   expires: new Date(Date.now() + 900000)
+      
+    // });
+    // response.cookie("login_token", data.login_token, {
+    //   httpOnly: false,
+    //   secure: false,
+    //   sameSite: "none",
+    //   maxAge: - 1,
+    //   domain: COOKIE_DOMAIN,
+    //   path: '/',
+    //   expires: new Date(Date.now() + 900000)
+    // });
+    // response.setHeader('Set-Cookie', `login_token=${data.login_token}; HttpOnly`);
+
+    response.setHeader('Set-Cookie', `auth_token=${data.auth_token}; HttpOnly; Domain=${COOKIE_DOMAIN}; Secure=false; SameSite=None; Path=/`);
+    response.setHeader('Set-Cookie', `login_token=${data.login_token}; Domain=${COOKIE_DOMAIN}; Secure=false; SameSite=None; Path=/`);
 
     return response.status(200).json(data);
   }
